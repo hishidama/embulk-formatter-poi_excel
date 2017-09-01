6cc061378b81c28585e5429f5020ba091ba2a316
yuokada <callistoiv+git@gmail.com>
2017-09-02 00:27:02 +0900

    Reformat code

--- src/main/java/org/embulk/formatter/poi_excel/PoiExcelFormatterPlugin.java
+++ src/main/java/org/embulk/formatter/poi_excel/PoiExcelFormatterPlugin.java
@@ -1,9 +1,6 @@
 package org.embulk.formatter.poi_excel;
 
-import java.io.IOException;
-import java.text.MessageFormat;
-import java.util.Map;
-
+import com.google.common.base.Optional;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.SpreadsheetVersion;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -28,100 +25,116 @@ import org.embulk.spi.time.TimestampFormatter.TimestampColumnOption;
 import org.embulk.spi.util.FileOutputOutputStream;
 import org.embulk.spi.util.FileOutputOutputStream.CloseMode;
 
-import com.google.common.base.Optional;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Map;
 
-public class PoiExcelFormatterPlugin implements FormatterPlugin {
-	public interface PluginTask extends Task, TimestampFormatter.Task {
-		@Config("spread_sheet_version")
-		@ConfigDefault("\"EXCEL2007\"")
-		public SpreadsheetVersion getSpreadsheetVersion();
+public class PoiExcelFormatterPlugin
+        implements FormatterPlugin
+{
+    public interface PluginTask
+            extends Task, TimestampFormatter.Task
+    {
+        @Config("spread_sheet_version")
+        @ConfigDefault("\"EXCEL2007\"")
+        public SpreadsheetVersion getSpreadsheetVersion();
 
-		@Config("sheet_name")
-		@ConfigDefault("\"Sheet1\"")
-		public String getSheetName();
+        @Config("sheet_name")
+        @ConfigDefault("\"Sheet1\"")
+        public String getSheetName();
 
-		@Config("column_options")
-		@ConfigDefault("{}")
-		public Map<String, ColumnOption> getColumnOptions();
+        @Config("column_options")
+        @ConfigDefault("{}")
+        public Map<String, ColumnOption> getColumnOptions();
 
-		@ConfigInject
-		public BufferAllocator getBufferAllocator();
-	}
+        @ConfigInject
+        public BufferAllocator getBufferAllocator();
+    }
 
-	public interface ColumnOption extends Task, TimestampColumnOption {
+    public interface ColumnOption
+            extends Task, TimestampColumnOption
+    {
 
-		@Config("data_format")
-		@ConfigDefault("null")
-		public Optional<String> getDataFormat();
-	}
+        @Config("data_format")
+        @ConfigDefault("null")
+        public Optional<String> getDataFormat();
+    }
 
-	@Override
-	public void transaction(ConfigSource config, Schema schema, FormatterPlugin.Control control) {
-		PluginTask task = config.loadConfig(PluginTask.class);
+    @Override
+    public void transaction(ConfigSource config, Schema schema, FormatterPlugin.Control control)
+    {
+        PluginTask task = config.loadConfig(PluginTask.class);
 
-		control.run(task.dump());
-	}
+        control.run(task.dump());
+    }
 
-	@Override
-	public PageOutput open(TaskSource taskSource, final Schema schema, FileOutput output) {
-		final PluginTask task = taskSource.loadTask(PluginTask.class);
+    @Override
+    public PageOutput open(TaskSource taskSource, final Schema schema, FileOutput output)
+    {
+        final PluginTask task = taskSource.loadTask(PluginTask.class);
 
-		final Sheet sheet = newWorkbook(task);
+        final Sheet sheet = newWorkbook(task);
 
-		final FileOutputOutputStream stream = new FileOutputOutputStream(output, task.getBufferAllocator(),
-				CloseMode.CLOSE);
-		stream.nextFile();
+        final FileOutputOutputStream stream = new FileOutputOutputStream(output, task.getBufferAllocator(),
+                CloseMode.CLOSE);
+        stream.nextFile();
 
-		return new PageOutput() {
-			private final PageReader pageReader = new PageReader(schema);
+        return new PageOutput()
+        {
+            private final PageReader pageReader = new PageReader(schema);
 
-			@Override
-			public void add(Page page) {
-				pageReader.setPage(page);
-				PoiExcelColumnVisitor visitor = new PoiExcelColumnVisitor(task, schema, sheet, pageReader);
-				while (pageReader.nextRecord()) {
-					schema.visitColumns(visitor);
-					visitor.endRecord();
-				}
-			}
+            @Override
+            public void add(Page page)
+            {
+                pageReader.setPage(page);
+                PoiExcelColumnVisitor visitor = new PoiExcelColumnVisitor(task, schema, sheet, pageReader);
+                while (pageReader.nextRecord()) {
+                    schema.visitColumns(visitor);
+                    visitor.endRecord();
+                }
+            }
 
-			@Override
-			public void finish() {
-				Workbook book = sheet.getWorkbook();
-				try (FileOutputOutputStream os = stream) {
-					book.write(os);
-					os.finish();
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			}
+            @Override
+            public void finish()
+            {
+                Workbook book = sheet.getWorkbook();
+                try (FileOutputOutputStream os = stream) {
+                    book.write(os);
+                    os.finish();
+                }
+                catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
 
-			@Override
-			public void close() {
-				stream.close();
-			}
-		};
-	}
+            @Override
+            public void close()
+            {
+                stream.close();
+            }
+        };
+    }
 
-	@SuppressWarnings("resource")
-	protected Sheet newWorkbook(PluginTask task) {
-		Workbook book;
-		{
-			SpreadsheetVersion version = task.getSpreadsheetVersion();
-			switch (version) {
-			case EXCEL97:
-				book = new HSSFWorkbook();
-				break;
-			case EXCEL2007:
-				book = new XSSFWorkbook();
-				break;
-			default:
-				throw new UnsupportedOperationException(MessageFormat.format("unsupported spread_sheet_version={0}",
-						version));
-			}
-		}
+    @SuppressWarnings("resource")
+    protected Sheet newWorkbook(PluginTask task)
+    {
+        Workbook book;
+        {
+            SpreadsheetVersion version = task.getSpreadsheetVersion();
+            switch (version) {
+                case EXCEL97:
+                    book = new HSSFWorkbook();
+                    break;
+                case EXCEL2007:
+                    book = new XSSFWorkbook();
+                    break;
+                default:
+                    throw new UnsupportedOperationException(MessageFormat.format("unsupported spread_sheet_version={0}",
+                            version));
+            }
+        }
 
-		String sheetName = task.getSheetName();
-		return book.createSheet(sheetName);
-	}
+        String sheetName = task.getSheetName();
+        return book.createSheet(sheetName);
+    }
 }
--- src/main/java/org/embulk/formatter/poi_excel/visitor/PoiExcelColumnVisitor.java
+++ src/main/java/org/embulk/formatter/poi_excel/visitor/PoiExcelColumnVisitor.java
@@ -1,9 +1,6 @@
 package org.embulk.formatter.poi_excel.visitor;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
+import com.google.common.base.Optional;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
@@ -22,133 +19,149 @@ import org.embulk.spi.time.TimestampFormatter;
 import org.embulk.spi.util.Timestamps;
 import org.joda.time.DateTime;
 
-import com.google.common.base.Optional;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-public class PoiExcelColumnVisitor implements ColumnVisitor {
+public class PoiExcelColumnVisitor
+        implements ColumnVisitor
+{
 
-	private final PluginTask task;
-	private final Schema schema;
-	private final Sheet sheet;
-	private final PageReader pageReader;
+    private final PluginTask task;
+    private final Schema schema;
+    private final Sheet sheet;
+    private final PageReader pageReader;
 
-	private int rowIndex = 0;
+    private int rowIndex = 0;
 
-	private Row currentRow = null;
+    private Row currentRow = null;
 
-	public PoiExcelColumnVisitor(PluginTask task, Schema schema, Sheet sheet, PageReader pageReader) {
-		this.task = task;
-		this.schema = schema;
-		this.sheet = sheet;
-		this.pageReader = pageReader;
-	}
+    public PoiExcelColumnVisitor(PluginTask task, Schema schema, Sheet sheet, PageReader pageReader)
+    {
+        this.task = task;
+        this.schema = schema;
+        this.sheet = sheet;
+        this.pageReader = pageReader;
+    }
 
-	@Override
-	public void booleanColumn(Column column) {
-		if (pageReader.isNull(column)) {
-			return;
-		}
-		boolean value = pageReader.getBoolean(column);
-		Cell cell = getCell(column);
-		cell.setCellValue(value);
-	}
+    @Override
+    public void booleanColumn(Column column)
+    {
+        if (pageReader.isNull(column)) {
+            return;
+        }
+        boolean value = pageReader.getBoolean(column);
+        Cell cell = getCell(column);
+        cell.setCellValue(value);
+    }
 
-	@Override
-	public void longColumn(Column column) {
-		if (pageReader.isNull(column)) {
-			return;
-		}
-		long value = pageReader.getLong(column);
-		Cell cell = getCell(column);
-		cell.setCellValue(value);
-	}
+    @Override
+    public void longColumn(Column column)
+    {
+        if (pageReader.isNull(column)) {
+            return;
+        }
+        long value = pageReader.getLong(column);
+        Cell cell = getCell(column);
+        cell.setCellValue(value);
+    }
 
-	@Override
-	public void doubleColumn(Column column) {
-		if (pageReader.isNull(column)) {
-			return;
-		}
-		double value = pageReader.getDouble(column);
-		Cell cell = getCell(column);
-		cell.setCellValue(value);
-	}
+    @Override
+    public void doubleColumn(Column column)
+    {
+        if (pageReader.isNull(column)) {
+            return;
+        }
+        double value = pageReader.getDouble(column);
+        Cell cell = getCell(column);
+        cell.setCellValue(value);
+    }
 
-	@Override
-	public void stringColumn(Column column) {
-		if (pageReader.isNull(column)) {
-			return;
-		}
-		String value = pageReader.getString(column);
-		Cell cell = getCell(column);
-		cell.setCellValue(value);
-	}
+    @Override
+    public void stringColumn(Column column)
+    {
+        if (pageReader.isNull(column)) {
+            return;
+        }
+        String value = pageReader.getString(column);
+        Cell cell = getCell(column);
+        cell.setCellValue(value);
+    }
 
-	@Override
-	public void jsonColumn(Column column) {
-	    throw new UnsupportedOperationException("This plugin doesn't support json type. Please try to upgrade version of the plugin using 'embulk gem update' command. If the latest version still doesn't support json type, please contact plugin developers, or change configuration of input plugin not to use json type.");
-	}
+    @Override
+    public void jsonColumn(Column column)
+    {
+        throw new UnsupportedOperationException("This plugin doesn't support json type. Please try to upgrade version of the plugin using 'embulk gem update' command. If the latest version still doesn't support json type, please contact plugin developers, or change configuration of input plugin not to use json type.");
+    }
 
-	@Override
-	public void timestampColumn(Column column) {
-		if (pageReader.isNull(column)) {
-			return;
-		}
-		TimestampFormatter formatter = getTimestampFormatter(column);
-		Timestamp timestamp = pageReader.getTimestamp(column);
-		DateTime dateTime = new DateTime(timestamp.toEpochMilli(), formatter.getTimeZone());
-		Date value = dateTime.toDate();
-		Cell cell = getCell(column);
-		cell.setCellValue(value);
-	}
+    @Override
+    public void timestampColumn(Column column)
+    {
+        if (pageReader.isNull(column)) {
+            return;
+        }
+        TimestampFormatter formatter = getTimestampFormatter(column);
+        Timestamp timestamp = pageReader.getTimestamp(column);
+        DateTime dateTime = new DateTime(timestamp.toEpochMilli(), formatter.getTimeZone());
+        Date value = dateTime.toDate();
+        Cell cell = getCell(column);
+        cell.setCellValue(value);
+    }
 
-	private TimestampFormatter[] timestampFormatters;
+    private TimestampFormatter[] timestampFormatters;
 
-	protected final TimestampFormatter getTimestampFormatter(Column column) {
-		if (timestampFormatters == null) {
-			timestampFormatters = Timestamps.newTimestampColumnFormatters(task, schema, task.getColumnOptions());
-		}
-		return timestampFormatters[column.getIndex()];
-	}
+    protected final TimestampFormatter getTimestampFormatter(Column column)
+    {
+        if (timestampFormatters == null) {
+            timestampFormatters = Timestamps.newTimestampColumnFormatters(task, schema, task.getColumnOptions());
+        }
+        return timestampFormatters[column.getIndex()];
+    }
 
-	protected Cell getCell(Column column) {
-		Cell cell = CellUtil.getCell(getRow(), column.getIndex());
+    protected Cell getCell(Column column)
+    {
+        Cell cell = CellUtil.getCell(getRow(), column.getIndex());
 
-		ColumnOption option = getColumnOption(column);
-		if (option != null) {
-			Optional<String> formatOption = option.getDataFormat();
-			if (formatOption.isPresent()) {
-				String formatString = formatOption.get();
-				CellStyle style = styleMap.get(formatString);
-				if (style == null) {
-					Workbook book = sheet.getWorkbook();
-					style = book.createCellStyle();
-					CreationHelper helper = book.getCreationHelper();
-					short fmt = helper.createDataFormat().getFormat(formatString);
-					style.setDataFormat(fmt);
-					styleMap.put(formatString, style);
-				}
-				cell.setCellStyle(style);
-			}
-		}
+        ColumnOption option = getColumnOption(column);
+        if (option != null) {
+            Optional<String> formatOption = option.getDataFormat();
+            if (formatOption.isPresent()) {
+                String formatString = formatOption.get();
+                CellStyle style = styleMap.get(formatString);
+                if (style == null) {
+                    Workbook book = sheet.getWorkbook();
+                    style = book.createCellStyle();
+                    CreationHelper helper = book.getCreationHelper();
+                    short fmt = helper.createDataFormat().getFormat(formatString);
+                    style.setDataFormat(fmt);
+                    styleMap.put(formatString, style);
+                }
+                cell.setCellStyle(style);
+            }
+        }
 
-		return cell;
-	}
+        return cell;
+    }
 
-	protected final ColumnOption getColumnOption(Column column) {
-		Map<String, ColumnOption> map = task.getColumnOptions();
-		return map.get(column.getName());
-	}
+    protected final ColumnOption getColumnOption(Column column)
+    {
+        Map<String, ColumnOption> map = task.getColumnOptions();
+        return map.get(column.getName());
+    }
 
-	private Map<String, CellStyle> styleMap = new HashMap<>();
+    private Map<String, CellStyle> styleMap = new HashMap<>();
 
-	private Row getRow() {
-		if (currentRow == null) {
-			currentRow = sheet.createRow(rowIndex);
-		}
-		return currentRow;
-	}
+    private Row getRow()
+    {
+        if (currentRow == null) {
+            currentRow = sheet.createRow(rowIndex);
+        }
+        return currentRow;
+    }
 
-	public void endRecord() {
-		rowIndex++;
-		currentRow = null;
-	}
+    public void endRecord()
+    {
+        rowIndex++;
+        currentRow = null;
+    }
 }
